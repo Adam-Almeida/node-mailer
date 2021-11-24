52fb7795e25d3b9a03c601683a49987794681426
Adam Almeida <adamportifolio@gmail.com>
2021-11-23 22:42:10 -0300

    Criando a classe de provider de email

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,13 +1,38 @@
+import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 import { IUserRepository } from "../../repositories/IUserRepository";
+import { ICreateUserDTO } from "./CreateUserDTO";
 
 class CreateUserUseCase{
-    constructor(private userRepository: IUserRepository){};
+    constructor(
+      private userRepository: IUserRepository,
+      private mailProvider: IMailProvider  
+    ){};
 
-    async execute(){
-      this.userRepository.findByEmail();
+    async execute(data: ICreateUserDTO){
+      const userAlreadyExists = this.userRepository.findByEmail(data.email);
 
+      if(userAlreadyExists){
+        throw new Error("User Already Exists.")
+      }
+
+      const user = new User(data);
+
+      await this.userRepository.save(user);
+
+      await this.mailProvider.sendMail({
+        to:{
+          name: data.name,
+          email: data.email
+        },
+        from:{
+          name: "Adam Almeida",
+          email: "adam@adamalmeida.com"
+        },
+        subject: "Teste de Email",
+        body: "Teste de envio de email via app."
+      })
   }
-
 }
 
 export { CreateUserUseCase };
